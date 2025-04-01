2b950a75c3445c50fcc85982bc657720198cd1b6
YenCheng Lai <yenchenglai0618@gmail.com>
2025-04-01 17:06:22 +0800

    adjust bcrpyt and connection issue between services

--- backend/expense-service/app/main.py
+++ backend/expense-service/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from .models import ExpenseCreate, ExpenseResponse
-from .auth import get_current_user
+from .auth import get_current_user, TokenData
 from .db import get_db  # Import from db.py
 from bson import ObjectId
 
@@ -27,6 +27,6 @@ async def create_expense(expense: ExpenseCreate, current_user: dict = Depends(ge
     return ExpenseResponse(**expense_dict)
 
 @app.get("/expense", response_model=list[ExpenseResponse])
-async def get_expenses(current_user: dict = Depends(get_current_user), db=Depends(get_db)):
-    expenses = db.expense.find({"userId": current_user["userId"]})
+async def get_expenses(current_user: TokenData = Depends(get_current_user), db=Depends(get_db)):
+    expenses = db.expense.find({"userId": current_user.userId})
     return [ExpenseResponse(**{**exp, "_id": str(exp["_id"]), "userId": str(exp["userId"])}) for exp in expenses]
